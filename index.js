89da8e5f184e21153cf2903190f3468db5bd2c05
insanenerd <thelokeshgoel00@gmail.com>
2020-07-17 22:53:21 +0530

    Navbar Complete

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 let mainNav = document.getElementById("main-nav");
 let navBarToggle = document.getElementById("js-navbar-toggle");
-
+let navBarClose = document.getElementById("js-navbar-close")
 navBarToggle.addEventListener("click", function() {
     console.log("working")
     mainNav.classList.toggle("nav-active");
+    navBarToggle.classList.toggle("vis-bar");
+    navBarClose.classList.toggle("vis-close");
+});
+navBarClose.addEventListener("click", function() {
+    console.log("working")
+    mainNav.classList.toggle("nav-active");
+    navBarToggle.classList.toggle("vis-bar");
+    navBarClose.classList.toggle("vis-close");
 });
